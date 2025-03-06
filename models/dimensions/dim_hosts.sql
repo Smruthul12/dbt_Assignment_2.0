d363a79544ec76af24906904c0fcc1256779e952
Smruthul12 <msmruthul2002@gmail.com>
2025-03-06 10:05:04 +0530

    Dimensions models done

---
+++ models/dimensions/dim_hosts.sql
@@ -0,0 +1,11 @@
+SELECT
+    host_id,
+    host_name,
+    host_total_listings_count AS total_listings,
+    host_response_rate::NUMBER / 100 AS response_rate,  -- Convert percentage
+    CASE 
+        WHEN host_is_superhost = 't' THEN TRUE 
+        ELSE FALSE 
+    END AS is_superhost
+FROM {{ ref('stg_listings') }}
+GROUP BY 1, 2, 3, 4, 5
---
+++ models/dimensions/dim_reviews.sql
@@ -0,0 +1,19 @@
+WITH review_stats AS (
+    SELECT
+        listing_id,
+        MIN(date) AS first_review,
+        MAX(date) AS last_review,
+        COUNT(*) AS total_reviews
+    FROM {{ ref('stg_reviews') }}
+    GROUP BY listing_id
+)
+
+SELECT
+    rs.listing_id,
+    rs.first_review,
+    rs.last_review,
+    rs.total_reviews,
+    l.review_scores_rating  -- Getting rating from `stg_listings`
+FROM review_stats rs
+LEFT JOIN {{ ref('stg_listings') }} l
+    ON rs.listing_id = l.listing_id
