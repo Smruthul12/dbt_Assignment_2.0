22a84926e9def5318ba4fcb4c5ddc8a14b4cf8c8
Smruthul12 <Smruthul12@users.noreply.github.com>
2025-03-07 07:40:54 +0000

    deploy: c8b9ff39591cfb6aba0bbff5ce6165ce30291206

--- compiled/dbt_assignment_2/models/marts/dimensions/dim_hosts.sql
+++ compiled/dbt_assignment_2/models/marts/dimensions/dim_hosts.sql
@@ -1,11 +1,24 @@
-SELECT
-    host_id,
-    host_name,
-    host_total_listings_count AS total_listings,
-    host_response_rate::NUMBER / 100 AS response_rate,  -- Convert percentage
-    CASE 
-        WHEN host_is_superhost = 't' THEN TRUE 
-        ELSE FALSE 
-    END AS is_superhost
-FROM AIRBNB.PROD.stg_listings
-GROUP BY 1, 2, 3, 4, 5
+
+WITH ranked_hosts AS (
+    SELECT
+        host_id,
+        host_name,
+        host_total_listings_count AS total_listings,
+        host_response_rate::NUMBER / 100 AS response_rate,  -- Convert percentage
+        CASE 
+            WHEN host_is_superhost = 't' THEN TRUE 
+            ELSE FALSE 
+        END AS is_superhost,
+        CAST(updated_date AS TIMESTAMP_NTZ) AS updated_date,
+        ROW_NUMBER() OVER (PARTITION BY host_id ORDER BY updated_date DESC) AS row_num
+    FROM AIRBNB.PROD.stg_listings
+)
+SELECT 
+    host_id, 
+    host_name, 
+    total_listings, 
+    response_rate, 
+    is_superhost, 
+    updated_date
+FROM ranked_hosts
+WHERE row_num = 1
